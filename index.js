6f231c4d9ac9e350db625ac4fb61c023daba5880
kidus7 <kidusanteneh7@gmail.com>
2019-08-09 05:24:10 -0700

    modifing index

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var ejs = require('ejs')
-require('dotenv').config()
+var express = require("express");
+var bodyParser = require("body-parser");
+var ejs = require("ejs");
+require("dotenv").config();
+const moment = require("moment");
+const uuid = require("uuid/v1");
 
-var HelloCash = require('./hellocash')
-
-var app = express()
-
-app.use(bodyParser.json())
+var Eth_phonePrefix = "+2519"; //TODO: replace this with regex
+var app = express();
+var HelloCash = require("./hellocash");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 5000;
 
-app.get('/', function (req, res) {
-  console.log(req)
+app.get("/", function(req, res) {
+  console.log(req);
   res.status(200).json({
-    message: 'Welcome'
-  })
-})
+    message: "Welcome"
+  });
+});
 
-app.post('/', function (req, res) {
-  var data = req.body
-  var token = req.headers.token
+app.get("/form", function(req, res, next) {
+  res.render("form1.ejs", { message: " working!" });
+});
 
-  console.log(data)
-  if (token === '123456t') {
-    res.status(200).json({
-      message: 'Data has been added',
-      data: data
-    })
-  } else {
-    res.status(403).json({
-      status: 'FORBIDEN',
-      message: 'You are not allowed'
-
-    })
+app.post("/form", function(req, res, next) {
+  body = req.body;
+  if (Object.keys(body).includes("phone")) {
+    body.phone = [
+      Eth_phonePrefix,
+      body.phone.substr(body.phone.length - 8, 9)
+    ].join("");
   }
+  let today = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Africa/Nairobi" })
+  );
 
-})
+  var payload = {
+    from: body.phone,
+    amount: parseFloat(body.amount),
+    description: body.desc,
+    expires: moment(today)
+      .add(24, "hours")
+      .toISOString(),
+    notifyfrom: true,
+    notifyto: true,
+    currency: "ETB",
+    tracenumber: uuid()
+  };
 
-//HelloCash services
-app.get('/invoices', (req, res) => {
-  var who = req.query.who || 'kidus'
-  HelloCash.getInvoices(who)
-    .then(invoices => {
-      res.render('invoices.html.ejs', { invoices: invoices })
-      // console.log('SERVICE', invoices)
-      // res.setHeader('Content-Type', 'application/json')
-      // res.status(200).json(invoices)
-
+  // res.status(200).json(payload);
+  let who = body.radio;
+  console.log(who);
+  HelloCash.createInvoice(who, payload)
+    .then(response => {
+      // res.status(200).json(response);
+      res.render("invoice_ok.html.ejs", { data: response });
+      console.log("works");
+      //res.render("invoices_ok.html.ejs", { invoices: HelloCash.getInvoices() });
     })
     .catch(err => {
-      console.log(err)
-      res.status(500).json(err)
-    })
-})
-app.get('/account', (req, res) => {
-  var who = req.query.who || 'kidus'
-  HelloCash.account(who).then(data => {
-    res.render('account.html.ejs', { account: data })
-  })
-})
+      // console.log("here is an error");
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-app.listen(port, function () {
-  console.log(`Server listening on port: ${port}`)
-})
+app.listen(port, function() {
+  console.log(`Server listening on port: ${port}`);
+});
