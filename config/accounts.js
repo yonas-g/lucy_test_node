cfd97745f5ea3de4f06a9f4aa090efa1c93a4b9b
kidus7 <kidusanteneh7@gmail.com>
2019-08-09 05:26:45 -0700

    i changed this

--- config/accounts.js
+++ config/accounts.js
@@ -1,4 +1,7 @@
-var baseURL = 'https://api-et.hellocash.net'
+var path = require("path");
+require("dotenv").config({ path: path.resolve("./config/.env") });
+
+var baseURL = "https://api-et.hellocash.net";
 module.exports = {
   api: {
     authenticate: `${baseURL}/authenticate`,
@@ -6,20 +9,20 @@ module.exports = {
     accounts: `${baseURL}/accounts`
   },
   kidus: {
-    "principal": process.env.KIDUS_PRINCIPAL,
-    "credentials": process.env.KIDUS_CREDENTIALS,
-    "system": process.env.SYSTEM
+    principal: process.env.KIDUS_PRINCIPAL,
+    credentials: process.env.KIDUS_CREDENTIALS,
+    system: process.env.KIDUS_SYSTEM
   },
   yemane: {
-    "principal": process.env.YEMANE_PRINCIPAL,
-    "credentials": process.env.YEMANE_CREDENTIALS,
-    "system": process.env.SYSTEM
+    principal: process.env.YEMANE_PRINCIPAL,
+    credentials: process.env.YEMANE_CREDENTIALS,
+    system: process.env.YEMANE_SYSTEM
   },
   customers: {
     kaleab: "+251924102910",
     yonas: "+251924322570",
     leul: "+251983064336",
-    philiphos: "+251944233162",
-    anduamlak: "+251933905958",
+    philipos: "+251944233162",
+    anduamlak: "+251933905958"
   }
-}
+};
--- hellocash.js
+++ hellocash.js
@@ -1,71 +1,98 @@
-var config = require('./config/accounts')
-var rp = require('request-promise-native')
+var config = require("./config/accounts");
+var rp = require("request-promise-native");
+var express = require("express");
+var app = express();
 
-var self = module.exports = {
-    login: (who) => {
-        return new Promise((resolve, reject) => {
-            if (Object.keys(config).includes(who)) {
-                var options = {
-                    url: config.api.authenticate,
-                    method: 'POST',
-                    body: config[who],
-                    json: true
-                }
-                rp(options)
-                    .then(data => {
-                        console.log(data)
-                        resolve(data.token)
-                    })
-                    .catch(err => {
-                        reject(err)
-                    })
-            } else {
-                reject({
-                    message: `Unknown account ${who}`
-                })
+var self = (module.exports = {
+  login: who => {
+    return new Promise((resolve, reject) => {
+      if (Object.keys(config).includes(who)) {
+        var options = {
+          url: config.api.authenticate,
+          method: "POST",
+          body: config[who],
+          json: true
+        };
+        rp(options)
+          .then(function(data) {
+            console.log(data);
+            resolve(data.token);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      } else {
+        reject({
+          message: `Unknown account ${who}`
+        });
+      }
+    });
+  },
+  getInvoices: whose => {
+    return new Promise((resolve, reject) => {
+      self
+        .login(whose)
+        .then(token => {
+          var options = {
+            url: config.api.invoices,
+            method: "GET",
+            json: true,
+            headers: {
+              authorization: `Bearer ${token}`
             }
+          };
+          return rp(options);
         })
-    },
-    getInvoices: (whose) => {
-        return new Promise((resolve, reject) => {
-            self.login(whose)
-                .then(token => {
-                    var options = {
-                        url: config.api.invoices,
-                        method: 'GET',
-                        json: true,
-                        headers: {
-                            authorization: `Bearer ${token}`
-                        }
-                    }
-                    return rp(options)
-                })
-                .then(invoices => {
-                    console.log(invoices)
-                    resolve(invoices)
-                })
-                .catch(error => {
-                    reject(error)
-                })
+        .then(invoices => {
+          console.log(invoices);
+          resolve(invoices);
         })
-    },
-    account: (who) => {
-        return new Promise((resolve, reject) => {
-            self.login(who).then(token => {
-                var options = {
-                    url: config.api.accounts,
-                    json: true,
-                    method: 'GET',
-                    headers: {
-                        authorization: `Bearer ${token}`
-                    }
-                }
-                return rp(options)
-            })
-                .then(accounts => {
-                    resolve(accounts[0])
-                })
-                .catch(err => reject(err))
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  account: who => {
+    return new Promise((resolve, reject) => {
+      self
+        .login(who)
+        .then(token => {
+          var options = {
+            url: config.api.accounts,
+            json: true,
+            method: "GET",
+            headers: {
+              authorization: `Bearer ${token}`
+            }
+          };
+          return rp(options);
         })
-    }
-}
+        .then(accounts => {
+          resolve(accounts[0]);
+          console.log(accounts[1]);
+        })
+        .catch(err => reject(err));
+    });
+  },
+  createInvoice: (who, payload) => {
+    return new Promise((resolve, reject) => {
+      self.login(who).then(token => {
+        let options = {
+          url: config.api.invoices,
+          json: true,
+          method: "POST",
+          headers: {
+            authorization: `Bearer ${token}`
+          },
+          body: payload
+        };
+
+        rp(options)
+          .then(Response => {
+            resolve(Response).then(res.status(200).json(Response));
+          })
+          .catch(error => reject(error));
+      });
+    });
+  }
+});
