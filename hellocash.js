15fa94a3b38ba03c8237bfb217d494ccbb6c05c3
phlipTilahun <35027246+phlipTilahun@users.noreply.github.com>
2019-08-16 16:16:37 +0300

    Add files via upload

--- hellocash.js
+++ hellocash.js
@@ -1,98 +1,114 @@
-var config = require("./config/accounts");
-var rp = require("request-promise-native");
-var express = require("express");
-var app = express();
+//var config = require('./accounts')
+const axios = require('axios')
+const baseURL = 'https://api-et.hellocash.net'
+const uuid = require('uuid/v1');
 
-var self = (module.exports = {
-  login: who => {
-    return new Promise((resolve, reject) => {
-      if (Object.keys(config).includes(who)) {
-        var options = {
-          url: config.api.authenticate,
-          method: "POST",
-          body: config[who],
-          json: true
-        };
-        rp(options)
-          .then(function(data) {
-            console.log(data);
-            resolve(data.token);
-          })
-          .catch(err => {
-            reject(err);
-          });
-      } else {
-        reject({
-          message: `Unknown account ${who}`
-        });
-      }
-    });
-  },
-  getInvoices: whose => {
-    return new Promise((resolve, reject) => {
-      self
-        .login(whose)
-        .then(token => {
-          var options = {
-            url: config.api.invoices,
-            method: "GET",
-            json: true,
-            headers: {
-              authorization: `Bearer ${token}`
-            }
-          };
-          return rp(options);
-        })
-        .then(invoices => {
-          console.log(invoices);
-          resolve(invoices);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
-  },
-  account: who => {
-    return new Promise((resolve, reject) => {
-      self
-        .login(who)
-        .then(token => {
-          var options = {
-            url: config.api.accounts,
-            json: true,
-            method: "GET",
-            headers: {
-              authorization: `Bearer ${token}`
-            }
-          };
-          return rp(options);
-        })
-        .then(accounts => {
-          resolve(accounts[0]);
-          console.log(accounts[1]);
-        })
-        .catch(err => reject(err));
-    });
-  },
-  createInvoice: (who, payload) => {
-    return new Promise((resolve, reject) => {
-      self.login(who).then(token => {
-        let options = {
-          url: config.api.invoices,
-          json: true,
-          method: "POST",
-          headers: {
-            authorization: `Bearer ${token}`
-          },
-          body: payload
-        };
+const hellocash = {
+    login: async (principal,credentials,system="lucy")=>{
+        let body = {
+            "principal": principal,
+            "credentials": credentials,
+            "system": system
+        }
+        return (await axios.post(`${baseURL}/authenticate`,body)).data.token;
+    },
+    getInvoices: async (param,token)=>{
+        let config = {
+            url: `${baseURL}/invoices`,
+            method: "get",
+            headers: {authorization:`Bearer ${token}`},
+            params: param
+         }
+         return (await axios(config)).data;
+    },
+    postInvoices: async (invoice,token)=>{
+        let config = {
+            url: `${baseURL}/invoices`,
+            method: "post",
+            headers: {authorization:`Bearer ${token}`},
+            data: invoice
+        }
+        return (await axios(config)).data; 
+    },
+    validateInvoice: async (invoice,token) =>{
+        let config = {
+            url: `${baseURL}/invoices/validate`,
+            method: "post",
+            headers: {authorization:`Bearer ${token}`},
+            data: invoice
+        }
+        return (await axios(config)).data; 
+    },
+    deleteInvoice: async (id,token)=>{
+        let config = {
+            url: `${baseURL}/invoices/${id}`,
+            method: "delete",
+            headers: {authorization:`Bearer ${token}`},
+        }  
+        return (await axios(config)).data;
+    },
+    getTransfers: async (param,token)=>{
+        let config = {
+            url: `${baseURL}/transfers`,
+            method: "get",
+            headers: {authorization:`Bearer ${token}`},
+            params: param
+        }
+        return (await axios(config)).data;  
+    },
+    postTransfer: async (invoice,token)=>{
+        let config = {
+            url: `${baseURL}/transfers`,
+            method: "post",
+            headers: {authorization:`Bearer ${token}`},
+            data: invoice
+        }
+        return (await axios(config)).data; 
+    },
+    validateTransfer: async (invoice,token)=>{
+        let config = {
+            url: `${baseURL}/transfers`,
+            method: "post",
+            headers: {authorization:`Bearer ${token}`},
+            data: invoice
+        }
+        return (await axios(config)).data;
+    },
+    authrizeTransfer: async (id,token)=>{
+        let config = {
+            url: `${baseURL}/transfers/authorize`,
+            method: "post",
+            headers: {authorization:`Bearer ${token}`},
+            data : [id]
+        }
+        return (await axios(config)).data;
+    }
+}
 
-        rp(options)
-          .then(Response => {
-            resolve(Response).then(res.status(200).json(Response));
-          })
-          .catch(error => reject(error));
-      });
-    });
-  }
+hellocash.login("1416563","lucy1234").then(res=>{
+    //console.log(res);
+    var invoice = {
+        "amount": 10,
+        "description": "",
+        "from": "+2519",
+        "currency": "ETB",
+        "tracenumber": uuid(),
+        "notifyfrom": true,
+        "notifyto": true,
+        "expires": ""
+    }
+    var transfer = {
+        "amount": 1,
+        "description": "",
+        "to": "+251944233167",
+        "currency": "ETB",
+        "tracenumber": uuid(),
+        "referenceid": uuid(),
+        "notifyfrom": true,
+        "notifyto": true
+    }
+    hellocash.validateTransfer(transfer,res).then(res=>console.log(res),err=>console.log(err.response.data));
+   
+},err=>{
+    console.log(err.data);
 });
